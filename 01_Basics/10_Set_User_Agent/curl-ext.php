15463ac932c7b02a58a6025534bce7c1f4ed13d4
Serge Andriichuk <andriichuk29@gmail.com>
2019-06-01 02:04:31 +0300

    Fix comment format

--- 01_Basics/10_Set_User_Agent/curl-ext.php
+++ 01_Basics/10_Set_User_Agent/curl-ext.php
@@ -6,8 +6,7 @@ curl_setopt_array($curlHandler, [
     CURLOPT_URL => 'https://postman-echo.com/get',
     CURLOPT_USERAGENT => 'Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1A537a Safari/419.3',
 
-    /**
-     * OR set header
+    /* OR set header
       CURLOPT_HTTPHEADER => [
         'User-Agent: Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1A537a Safari/419.3',
     ]*/
